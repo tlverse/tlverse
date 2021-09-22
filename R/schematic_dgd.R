de970cafd62d0e8a5543be2689be5bd12f258b8c
jrcoyle <jrcoyle@Jeremys-MacBook-Pro.local>
2021-09-22 12:53:06 -0700

    schematic plot bugfixes

--- R/schematic_dgd.R
+++ R/schematic_dgd.R
@@ -43,15 +43,14 @@ make_schematic_grid <- function(){
 #' @export
 plot_schematic <- function(schematic,
                            grid=NULL,
-                           meta=NULL){
-
-  if(missing(grid)){
-    grid <- schematic_grid
-  }
-
+                           meta=NULL,
+                           type="line"){
+  # TODO: maybe omit rug and lines if these are null
+  # TODO: have option for facet or line type for different estimators/truth
   if(missing(meta)){
     meta <- schematic_meta
   }
+
 
   point_alpha = 1
 
@@ -59,16 +58,33 @@ plot_schematic <- function(schematic,
   # viridis::viridis_pal()(4)[c(1,3,2,4)]
   cbPalette <- c("#440154FF", "#35B779FF", "#31688EFF", "#FDE725FF")
 
+  # TODO: fix X + Y labels (something is weird)
 
-  type_guide <- ifelse(length(unique(grid$type))>1,"legend","none")
-  plotobj <- ggplot(schematic,aes(x=W,y=Y, color=factor(A)))+
+  # basic plot
+  plot_obj <- ggplot(schematic,aes(x=W,y=Y, color=factor(A)))+
     geom_point(alpha=point_alpha)+
-    geom_line(data=grid,aes(linetype=type))+theme_bw()+
-    xlab("Covariate (W)")+ylab("Outcome (Y)")+
     scale_color_manual("Treatment Level (A)",values=cbPalette)+
-    scale_linetype_discrete("Estimation", guide = type_guide)+
-    scale_x_continuous(meta$lims$W)+
-    scale_y_continuous(meta$lims$Y)+
-    geom_rug(data=meta$tsm_0,aes(x=NULL, linetype=type), sides="r")
-  return(plotobj)
+    scale_x_continuous(name = "Covariate (W)", limits = meta$lims$W)+
+    scale_y_continuous(name = "Outcome (Y)", limits = meta$lims$Y)+
+    theme_bw()
+
+  # add likelihood estimation lines if providef
+  if(!missing(grid)){
+    type_guide <- ifelse(length(unique(grid$type))>1,"legend","none")
+
+    plot_obj <- plot_obj +
+      geom_line(data=grid,aes(linetype=type))+theme_bw()+
+      scale_linetype_discrete("Estimation", guide = type_guide)
+  }
+
+  if(!missing(meta)){
+    plot_obj <- plot_obj +
+      geom_rug(data=meta$tsm_0,aes(x=NULL, linetype=type), sides="r")
+  }
+
+  if(type=="facet"){
+    plot_obj <- plot_obj + facet_wrap(~type)
+  }
+
+  return(plot_obj)
 }
